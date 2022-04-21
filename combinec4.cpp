a60f937036911369e33330f4e22a490885063ca6
Wilylejardinier <wilylejardinier02@gmail.com>
2022-04-21 11:23:38 +0200

    avancé dans le cpp

--- combinec4.cpp
+++ combinec4.cpp
@@ -1,7 +1,44 @@
 //---------------------------------------------------------------------------
 
 #pragma hdrstop
-
+#include <string.h>
 #include "combinec4.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+// definition du constructeur
+combinec4::combinec4()
+{
+	strcpy(idFeux,"128");
+	strcpy(idMoteur,"0B6");
+}
+void combinec4::EnregistrerDonnee(char*id,char*donnee)
+  {
+   if (!strcmp (idFeux,id))
+
+
+  }
+
+void combinec4::CalculerRegime()
+  {
+
+  }
+
+void combinec4::CalculerVitesse()
+  {
+
+  }
+
+int combinec4::Vitesse()
+  {
+
+  }
+
+int combinec4::Regime()
+  {
+
+  }
+
+char*combinec4::DonneeCAN(char*id)
+  {
+
+  }
